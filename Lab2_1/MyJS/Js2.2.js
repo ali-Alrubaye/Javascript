8f60340d313d682bad61add45e2bcbbd20a25306
Ali <Ali@DESKTOP-67KIB1C>
2016-12-31 13:58:34 +0100

    Add lab3

--- Lab2_1/MyJS/Js2.2.js
+++ Lab2_1/MyJS/Js2.2.js
@@ -1,95 +1,127 @@
 window.onload = function () {
-
     var myInput = document.getElementById("gropInput"),
-        myAnvandare = document.getElementById("Anvandare"),
-        myForname = document.getElementById("Forname"),
-        myEfternamn = document.getElementById("Efternamn"),
-        myTelnu = document.getElementById("Telnummer"),
-        myStad = document.getElementById("Stad"),
-        myAge = document.getElementById("Alder"),
+        myAnvandare = document.getElementById("user").focus(),
+        myForname = document.getElementById("fName"),
+        myEfternamn = document.getElementById("lName"),
+        myTelnu = document.getElementById("tel"),
+        myStad = document.getElementById("stad"),
+        myAge = document.getElementById("age"),
         myFDiv = document.getElementById("divFel"),
         myTextarea = document.getElementById("show"),
         myButton = document.getElementById("button");
-       
-        myFDiv.style.visibility = "hidden";
-        myTextarea.style.visibility = "hidden";
-  
-     var  value,
-            i;
-    
-    // var Person = function (anvandare, forName, efterName, tel, stad, alder) {
-    //     var newPerson = {};
+    myFDiv.style.visibility = "hidden";
+    myTextarea.style.visibility = "hidden";
+    var itemForm = document.forms[0];
+    myButton.onclick = function () {
+        if (!myAge.value.match(/[a-zA-Z]+/) && myAge.value >= 10 && myAge.value < 100) {
+            myTextarea.style.visibility = "visible";
+            setTimeout(function () {
+                myTextarea.style.visibility = "hidden";
+            }, 5000);
+            myTextarea.style.backgroundColor = "#90EE90";
+            var i;
+            for (i = 0; i < itemForm.length; i = i + 1) {
+                if (itemForm[i].getAttribute("placeholder") != null) {
+                    myTextarea.innerHTML += itemForm[i].getAttribute("placeholder") + ": " + itemForm[i].value + "\n";
+                }
+            }
+        } else {
+            myFDiv.style.backgroundColor = "red";
+            myFDiv.style.visibility = "visible";
+            myFDiv.style.fontSize = "22px";
+            myFDiv.innerHTML = "Du måste skriva nummer mellan 10-100 i ålder fältet";
+            setTimeout(function () {
+                myFDiv.style.visibility = "hidden";
+            }, 5000);
+        }
+    }
+    //var Person = function (anvandare, forName, efterName, tel, stad, alder) {
+    //    var newPerson = {};
     //    newPerson.Anvandare = anvandare;
     //    newPerson.Forname = forName;
     //    newPerson.Efternamn = efterName;
     //    newPerson.Telnummer = tel;
     //    newPerson.Stad = stad;
     //    newPerson.Alder = alder;
-
     //    return newPerson;
     //};
-  
-    // var p = new Person(myAnvandare.value, myForname.value, myEfternamn.value, myTelnu.value, myStad.value, myAge.value
-    //);
-  
-    // //console.log(Person.value);
-    // //document.getElementById("gg").innerHTML = Person.Anvandare.value + Person.Forname + Person.Efternamn + Person.Telnummer + Person.Stad + Person.Alder + " ";
-    // document.getElementById("gg").innerHTML = p.Anvandare + p.Forname + p.Efternamn + p.Telnummer + p.Stad + p.Alder + " ";
-    // console.log(p.Anvandare + p.Forname + p.Efternamn + p.Telnummer + p.Stad + p.Alder + " ")
-   //console.log(person);
-    //for (i = 0; i < persons.length; i++) {
-    //    value = persons[i];
-        
-    //    document.getElementById("gg").innerHTML = value.Anvandare.value + value.Forname + value.Efternamn + value.Telnummer + value.Stad + value.Alder + " ";
+    var p = [
+        {
+            user: "Ali",
+            fName: "Ali",
+            lNamn: "Al-Rubaye",
+            tel: "01234567",
+            stad: "Helsingbog",
+            age: 37
+        },
+        {
+            user: "Aveen",
+            fName: "Aveen",
+            lNamn: "Al-Gaff",
+            tel: "01234567",
+            stad: "Helsingbog",
+            age: 35
+        },
+        {
+            user: "Dj",
+            fName: "Dj",
+            lNamn: "Jacksson",
+            tel: "01234567",
+            stad: "Malmö",
+            age: 40
+        }
+    ];
+    document.getElementById("buttonAdd").onclick = function () {
+        "use strict";
+        var userTarg = itemForm.firstElementChild.firstElementChild.value;
+        for (var i in p) {
+            if (p.hasOwnProperty(i)) {
+                if (userTarg === p[i].user) {
+                    var re = p[i];
+                    myForname.value = re.fName;
+                    myEfternamn.value = re.lNamn;
+                    myTelnu.value = re.tel;
+                    myStad.value = re.stad;
+                    myAge.value = re.age;
+                    //                    var ou = document.forms[0];
+                    //                    var ouu = ou.getElementsByTagName("input");
+                    //                    for (i = 0; i < ouu.length; i = i + 1) {
+                    //                        var ouuu = ouu[i].getAttribute("id");
+                    //                        //var jj = document.getElementById(ouuu);
+                    //                        if (re.hasOwnProperty(ouuu)) {
+                    //                            //console.log(re.hasOwnProperty(ouuu));
+                    //                           var v= document.getElementById(ouuu).setAttribute("value",re.value);
+                    //                            //console.log(jj);
+                    //                            //if (ouuu===re.prototype) { 
+                    //                            //  jj.value = re;
+                    //                          console.log(v);
+                    //                        //}
+                    //}
+                    //                    }
+                }
+                else {
+                    myFDiv.style.visibility = "visible";
+                    myFDiv.innerHTML = "användaren ej hittas";
+                    setTimeout(function () {
+                        myFDiv.style.visibility = "hidden";
+                    }, 5000);
+                }
+
+            }
+        }
+
+    }
+
+    //function inputId() {
+    //    var listId = {};
+    //    var ou = document.forms[0];
+    //    var inputField = ou.getElementsByTagName("input");
+    //    for (var y = 0; y < inputField.length; y = y + 1) {
+    //        var myId = inputField[y].getAttribute("id");
+    //        if (myId !==null) {
+    //            listId = myId;
+    //        }
+    //    }
     //}
 
-
-    //       var listPerson ={}, p=[];
-    //             listPerson.Anvandare=myAnvandare.value;
-    //             listPerson.Forname=myForname.value;
-    //             listPerson.Efternamn=myEfternamn.value;
-    //             listPerson.Telnummer=myTelnu.value;
-    //             listPerson.Stad=myStad.value;
-    //             listPerson.Alder=myAge.value;
-    // p.push({listPerson: listPerson});
-    // console.log(persons);
- 
-
-    myButton.onclick = function () {
-
-        //setTimeout(function () {
-        //    myTextarea.style.visibility = "visible";
-        //}, 5000);
-
-
-        if (!myAge.value.match(/[a-zA-Z]+/) && myAge.value > 1 && myAge.value < 100) {
-             myTextarea.style.visibility = "visible";
-          
-            setTimeout(function () {
-                myTextarea.style.visibility = "hidden";
-            }, 5000);
-            myTextarea.style.backgroundColor = "#90EE90";
-            myTextarea.innerHTML += myAnvandare.value;
-            myTextarea.innerHTML += myForname.value;
-            myTextarea.innerHTML += myEfternamn.value;
-            myTextarea.innerHTML += myTelnu.value;
-            myTextarea.innerHTML += myStad.value;
-            myTextarea.innerHTML += myAge.value;
-
-            var getElement = document.getElementById("gg");
-            var i;
-            var item = document.getElementsByTagName("input")
-
-            for (i = 0; i < item.length; i = i + 1) {
-                getElement.innerHTML += item[i].value + "<br>";
-            }
-
-        } else {
-            myFDiv.style.backgroundColor = "red";
-            myFDiv.style.visibility = "visible";
-            myFDiv.style.fontSize = "22px";
-
-        }
-    }
 };
-
--- LabEtt/MyJS/JSLabEtt.js
+++ LabEtt/MyJS/JSLabEtt.js
@@ -1,21 +1,27 @@
-﻿
-    function myFunction() {
+﻿window.onload = function () {
+    var inputForm = document.getElementById("submit");
+    inputForm.onclick = function() {
         var result = document.getElementById("Användare").value;
-
-        if (result == "") {
+        //var letters = /^[a-zA-Z]+$/;
+        if (result !== "" ) {
+            document.getElementById("demo").innerHTML = "Tack " + result + " vi har fåt det Mail";
+            document.getElementById("grop").style.visibility = "hidden";
+            document.getElementById("color").style.backgroundColor = "green";
+            
+        } else {
             alert("Du Måste Skriva Användare Name");
-            return fals;
         }
-        document.getElementById("grop").style.visibility = "hidden";
-        document.getElementById("color").style.backgroundColor = "green";
+    };
+    
+   
 
-        document.getElementById("demo").innerHTML = "Tack " + result + " vi har fåt det Mail";
-    }
-
-    function ChangeMeClick1() {
+    
+    document.getElementById("button2").onclick = function() {
         document.getElementById("Paragraph1").style.color = "#ff0000";
+
     }
-    function ChangeMeClick2() {
+    document.getElementById("button3").onclick = function () {
         document.getElementById("Paragraph2").style.color = "green";
         document.getElementById("Paragraph3").style.color = "yellow";
     }
+};
